d7313e5f01c87fc8b881a3a1f7f7a737782c2394
khaled <dev.khaledsaleh@gmail.com>
2023-03-19 11:30:25 -0700

    Fixed file name

--- public/loader.js
+++ public/loader.js
@@ -3,7 +3,7 @@ const spinner = document.getElementById('spinner');
 function submit() {
     const city = document
         .getElementById('city-textbox')
-        .value
+        .value;
     const radius = document
         .getElementById('radius-select')
         .value
@@ -18,31 +18,40 @@ function submit() {
         },
         body: JSON.stringify({url: url})
     }).then(data => {
-        
-        data.text().then(value => {
-            console.log(`value is ${value}`)
-            const downloadUrl = window.URL
-            .createObjectURL(new Blob([value]));
-        const link = document.createElement('a');
-        link.href = downloadUrl;
-        link.setAttribute('download', `${city}_${radius}_Mile(s)_data.csv`);
-        document
-            .body
-            .appendChild(link);
-        link.click();
-        submitBtn.disabled = false;
-        spinner.hidden = true;
-        })
-            
+
+        data
+            .text()
+            .then(value => {
+                console.log(`value is ${value}`)
+                const downloadUrl = window
+                    .URL
+                    .createObjectURL(new Blob([value]));
+                const link = document.createElement('a');
+                link.href = downloadUrl;
+                link.setAttribute('download', `${capitalizeFirstLetter(city)}_${radius}_Mile(s)_data.csv`);
+                document
+                    .body
+                    .appendChild(link);
+                link.click();
+                submitBtn.disabled = false;
+                spinner.hidden = true;
+            })
+
     });
+
     // console.log(data) data     .json()     .then(json => {         const link =
     // document.createElement('a');        // let json = JSON.stringify(data.json())
     //         blob = new Blob(json, {type: 'octet/stream'})         let downloadUrl
-    // = window             .URL             .createObjectURL(blob)
-    // link.href = downloadUrl         link.download = 'data.csv'
-    // link.click()         window             .URL
-    // .revokeObjectURL(url)     }) .then(d => {     link.href = d;
-    // link.setAttribute('download', 'data.csv');     document         .body
+    // = window             .URL             .createObjectURL(blob) link.href =
+    // downloadUrl         link.download = 'data.csv' link.click()   window
+    //    .URL .revokeObjectURL(url)     }) .then(d => { link.href = d;
+    // link.setAttribute('download', 'data.csv');     document  .body
     // .appendChild(link); link.click();     console.log(d) });
 
+}
+
+function capitalizeFirstLetter(string) {
+    return string
+        .charAt(0)
+        .toUpperCase() + string.slice(1);
 }
