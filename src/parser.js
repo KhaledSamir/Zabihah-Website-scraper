495ae69bbc1bb7b864822029c94aa394af1b783d
khaled <dev.khaledsaleh@gmail.com>
2023-03-19 11:01:13 -0700

    speed up the parser

--- src/parser.js
+++ src/parser.js
@@ -4,37 +4,55 @@ import Business from "./models/business.js";
 import CSVCreator from "./csv-creator.js";
 
 export default class Parser {
-  businessesArr = new Array();
+
   csvCreator = new CSVCreator();
   async loadFromUrl(url) {
-    const pageResponse = await axios.get(url);
-    const businesses = await this.getBusinesses(pageResponse.data)
-    const data = await this.csvCreator.createCSV(businesses)
+    const response = await axios(url);
+    const links = await this.getBusinessesLinks(response.data)
+    const businesses = await this.getBusinessesInformation(links)
+    const data = await this
+      .csvCreator
+      .createCSV(businesses)
     return data;
   }
 
-  async getBusinesses(pageResponse) {
-    let $ = cheerio.load(pageResponse);
-    let businesses = $('div.titleBS > a')
-    if(businesses.length == 0)
-      throw "The url has no businesss";
-      
-    for(let i = 0; i < businesses.length;i++) {
-        const link = await axios.get(businesses[i].attribs.href)
-        const bizData = link.data;
-        let business = await this.createBiz(bizData)
-        this.businessesArr.push(business);
-    }
+  async getBusinessesInformation(links) {
+    const businessesArr = []
+    await Promise.all(links.map(async link => {
+      // axios.get(businesses[i].attribs.href)
+      const response = await axios.get(link)
+      const bizData = response.data;
+      let business = await this.createBiz(bizData);
+      businessesArr.push(business);
+    }))
 
-    return this.businessesArr;
-    
+    return businessesArr
   }
+
+  async getBusinessesLinks(pageResponse) {
+    let $ = cheerio.load(pageResponse);
+    let businessesLinks = []
+    $('div.titleBS > a', pageResponse).each((i, elm) => {
+      const link = $(elm).attr('href')
+      businessesLinks.push(link)
+    })
+    if (businessesLinks.length == 0) 
+      throw("The url has no businesss")
+    return businessesLinks;
+  }
+
+  // for (let i = 0; i < businesses.length; i++) {   const link = await  } return
+  // this.businessesLinks;
 
   async createBiz(bizData) {
     let $ = cheerio.load(bizData);
     const bizName = $('.titleBL').text()
-    const address = $('.bodyLink').first().text()
-    const phone = $('.midLink').first().text();
+    const address = $('.bodyLink')
+      .first()
+      .text()
+    const phone = $('.midLink')
+      .first()
+      .text();
     let business = new Business(bizName, address, phone)
     return business;
   }
